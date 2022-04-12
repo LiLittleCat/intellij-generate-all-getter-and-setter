3fa50f3bf641d46937162f9f0ffa95bef1f2f1f4
GitHub Action <action@github.com>
2022-04-12 11:18:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/lilittlecat/intellijgenerateallgetter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lilittlecat/intellijgenerateallgetter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lilittlecat.intellijgenerateallgetter.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lilittlecat.intellijgenerateallgetter.MyBundle
 
 class MyProjectService(project: Project) {
 
