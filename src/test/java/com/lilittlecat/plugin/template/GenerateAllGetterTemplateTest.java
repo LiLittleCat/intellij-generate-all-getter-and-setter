01bd87a7d9869b15cd4988cc4bdebc24890b0699
LiLittleCat <luoyukongchan@outlook.com>
2022-04-13 23:40:32 +0800

    Remove test.

--- src/test/java/com/lilittlecat/plugin/template/GenerateAllGetterTemplateTest.java
+++ src/test/java/com/lilittlecat/plugin/template/GenerateAllGetterTemplateTest.java
@@ -1,23 +1,11 @@
 package com.lilittlecat.plugin.template;
 
-import com.intellij.testFramework.LightPlatformCodeInsightTestCase;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixture4TestCase;
-import com.intellij.javaee.ProjectResources;
 
 /**
  * @author LiLittleCat
  * @since 4/13/2022
  */
-class GenerateAllGetterTemplateTest extends LightPlatformCodeInsightFixture4TestCase {
-    public void testWrapWithArray() throws Exception {
-        assertWrapping("{123}", "123");
-        assertWrapping("{null}", "null");
-    }
+public class GenerateAllGetterTemplateTest extends LightPlatformCodeInsightFixture4TestCase {
 
-    /**
-     * Asserts that the expansion of {@code content} by .o equals {@code expected}.
-     */
-    private void assertWrapping(String expected, String content) {
-
-    }
 }
